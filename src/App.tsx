9a53f393a255fd633fd3112873402570b7f86b1f
Alexandr Kurganov <alexandr.kurs.it@gmail.com>
2024-11-22 11:44:38 +0600

    Create header with input & button. Style header. Add axiosAPI.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 const App = () => {
 
     return (
-        <>
-            Hello!
-        </>
+        <div className="container">
+            <h1 className="text-center bg-black text-white text-uppercase py-2">To do list</h1>
+            <form className="my-3 mx-auto w-50 d-flex justify-content-between align-items-center">
+                <div className="w-75">
+                    <input type="text" className="form-control border-black bg-secondary text-white"/>
+                </div>
+                <button className="btn btn-dark">Add</button>
+            </form>
+            <hr/>
+        </div>
     )
 
 };
